6815d24b9c8ae221825347c9e14b216546126262
ashish silwal <80810085+aashish-jpg@users.noreply.github.com>
2022-02-27 09:07:42 -0800

    assignment

---
+++ client/src/core/fixedPrices.js
@@ -0,0 +1,32 @@
+export const prices = [
+  {
+    _id: 0,
+    name: 'Any',
+    array: [],
+  },
+  {
+    _id: 1,
+    name: '$0 to $19',
+    array: [0, 19],
+  },
+  {
+    _id: 2,
+    name: '$20 to $49',
+    array: [20, 49],
+  },
+  {
+    _id: 3,
+    name: '$50 to $99',
+    array: [50, 99],
+  },
+  {
+    _id: 4,
+    name: '$100 to $199',
+    array: [100, 199],
+  },
+  {
+    _id: 5,
+    name: 'More than $200',
+    array: [200, 10000],
+  },
+];
